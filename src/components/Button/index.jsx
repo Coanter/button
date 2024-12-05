0aba070c61b70aced98bf43dd2f8c139b0d219da
Timur Kazakov <kazakov_t@au.kg>
2024-12-05 10:55:55 +0600

    comm

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,11 @@
 import "./index.css"
 
 export function Button(props){
-    const {variant = "primary", children} = props;
-    const className = 'my-button my button--${variant}'
+    const { variant = "primary", size="md", children} = props;
+    const className = `my-button my-button--${variant}`;
+    
 
-    return <button className = {className}>{children}</button>
+    return <button className = {className}>{children}</button>;
 }
 
 export default Button;
