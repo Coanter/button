bcf6b527c01e7791e8827016aaf3c605ca3d4850
Coanter <coanterktd@gmail.com>
2024-12-13 05:01:02 +0600

    done

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,30 @@
 import "./index.css"
+import React from 'react';
 
-export function Button(props){
-    const { variant = "primary", size="md", children} = props;
-    const className = `my-button my-button--${variant}`;
-    
+export function Button(props) {
+    const {type="button", onClick, disabled=false, variant="primary", size="md", children} = props;
+    const className = `btn btn--${variant} btn--${size}`;
 
-    return <button className = {className}>{children}</button>;
+    const doNothing = () => {
+        console.log("This button did nothing...");
+    }
+
+    let onClickHandler;
+
+    switch (onClick) {
+        case "doNothing":
+            onClickHandler = doNothing;
+            break;
+        default:
+            if (typeof onClick !== "string"){
+                onClickHandler = onClick;
+            } else {
+                onClickHandler = doNothing;
+            }
+            break;
+    }
+
+    return <button type={type} className={className} onClick={onClickHandler} disabled={disabled}>{children}</button>;
 }
-// asd?
 
 export default Button;
